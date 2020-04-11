40c3cf434e2118826816976020b126009fbdf7da
alexjederock <60864509+alexjederock@users.noreply.github.com>
2020-04-11 07:45:39 -0400

    first commit

---
+++ orderBook.js
@@ -0,0 +1,48 @@
+function isOrderMatch (incomingOrder, existingOrder)
+{
+  if (incomingOrder.price === existingOrder.price &&
+    incomingOrder.type !== existingOrder.type)
+  {
+    return true
+  }
+  return false
+}
+
+function handleOrder (book, incomingOrder, index)
+{
+  if (incomingOrder.quantity === book[ index ].quantity)
+  {
+    book.splice(index, 1)
+
+    return book
+  } else if (incomingOrder.quantity > book[ index ].quantity)
+  {
+    incomingOrder.quantity -= book[ index ].quantity
+    book.splice(index, 1)
+
+    return reconcileOrder(book, incomingOrder)
+  } else
+  {
+    book[ index ].quantity -= incomingOrder.quantity
+
+    return book
+  }
+}
+
+
+function reconcileOrder (book, order)
+{
+  for (let i = 0; i < book.length; i++)
+  {
+    if (isOrderMatch(order, book[ i ]))
+    {
+      return handleOrder(book, order, i)
+    }
+  }
+  book.push(order)
+
+  return order
+
+}
+
+module.exports = reconcileOrder
